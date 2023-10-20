5aa599a5f1c883ede45a0fc63a47d4e71ea261d2
stykst <k_stoyanov@alice.it>
2023-10-20 08:58:11 +0300

    Adding new homework

---
+++ ProgrammingFundamentalsAndUnitTesting/Arrays and Lists - Exercise/01. Gauss' Trick/Program.cs
@@ -0,0 +1,22 @@
+﻿var numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+var n = numbers.Count;
+var first = 0;
+var last = n - 1;
+var newNumbers = new List<int>();
+
+for (int i = 0; i < n / 2; i++)
+{
+    int sum = numbers[first] + numbers[last];
+    newNumbers.Add(sum);
+
+    first++;
+    last--;
+
+    if (first == last) newNumbers.Add(numbers[first]);
+}
+
+Console.WriteLine(string.Join(" ", newNumbers));
