e04124377df0ce57553c5c925e3cfb1ee062a247
stykst <k_stoyanov@alice.it>
2023-10-30 17:30:23 +0200

    Refactoring

--- ProgrammingFundamentalsAndUnitTesting/Exam Preparation - 1/03. Center Point/TestApp.Tests/CenterPointTests.cs
+++ ProgrammingFundamentalsAndUnitTesting/Exam Preparation - 1/03. Center Point/TestApp.Tests/CenterPointTests.cs
@@ -8,30 +8,90 @@ public class CenterPointTests
     [Test]
     public void Test_GetClosest_WhenFirstPointIsCloser_ShouldReturnFirstPoint()
     {
-        // TODO: implement the test
+        // Arrange
+        var x1 = 0.9;
+        var x2 = 4.9;
+        var y1 = 0.1;
+        var y2 = 2.9;
+
+        var expected = (0.9, 0.1);
+
+        // Act
+        var result = CenterPoint.GetClosest(x1, y1, x2, y2);
+
+        // Assert
+        Assert.AreEqual(expected.ToString(), result);
     }
 
     [Test]
     public void Test_GetClosest_WhenSecondPointIsCloser_ShouldReturnSecondPoint()
     {
-        // TODO: implement the test
+        // Arrange
+        var x1 = 5.9;
+        var x2 = 4.9;
+        var y1 = 7.1;
+        var y2 = 2.9;
+
+        var expected = (4.9, 2.9);
+
+        // Act
+        var result = CenterPoint.GetClosest(x1, y1, x2, y2);
+
+        // Assert
+        Assert.AreEqual(expected.ToString(), result);
     }
 
     [Test]
     public void Test_GetClosest_WhenBothPointsHaveEqualDistance_ShouldReturnFirstPoint()
     {
-        // TODO: implement the test
+        // Arrange
+        var x1 = 1;
+        var x2 = 1;
+        var y1 = 2;
+        var y2 = 2;
+
+        var expected = (1, 2);
+
+        // Act
+        var result = CenterPoint.GetClosest(x1, y1, x2, y2);
+
+        // Assert
+        Assert.AreEqual(expected.ToString(), result);
     }
 
     [Test]
     public void Test_GetClosest_WhenFirstPointIsNegative_ShouldReturnFirstPoint()
     {
-        // TODO: implement the test
+        // Arrange
+        var x1 = -1;
+        var x2 = 1;
+        var y1 = -2;
+        var y2 = 2;
+
+        var expected = (-1, -2);
+
+        // Act
+        var result = CenterPoint.GetClosest(x1, y1, x2, y2);
+
+        // Assert
+        Assert.AreEqual(expected.ToString(), result);
     }
 
     [Test]
     public void Test_GetClosest_WhenSecondPointIsNegative_ShouldReturnSecondPoint()
     {
-        // TODO: implement the test
+        // Arrange
+        var x1 = 1;
+        var x2 = -1;
+        var y1 = 2;
+        var y2 = -2;
+
+        var expected = "(-1, -2)";
+
+        // Act
+        var result = CenterPoint.GetClosest(x1, y1, x2, y2);
+
+        // Assert
+        Assert.AreEqual(expected, result);
     }
 }
