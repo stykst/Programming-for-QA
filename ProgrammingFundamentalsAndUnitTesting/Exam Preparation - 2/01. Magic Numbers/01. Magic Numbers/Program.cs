7e4e98c2f31f72538e53dba5c3a176bdc536474f
stykst <k_stoyanov@alice.it>
2023-11-01 09:52:10 +0200

    Refactoring

--- ProgrammingFundamentalsAndUnitTesting/Exam Preparation - 2/01. Magic Numbers/01. Magic Numbers/Program.cs
+++ ProgrammingFundamentalsAndUnitTesting/Exam Preparation - 2/01. Magic Numbers/01. Magic Numbers/Program.cs
@@ -19,11 +19,6 @@ static bool IsPrime(int n)
     if (n <= 3) return true;
     if (n % 2 == 0 || n % 3 == 0) return false;
 
-    for (int i = 5; i * i <= n; i += 6)
-    {
-        if (n % i == 0 || n % (i + 2) == 0) return false;
-    }
-
     return true;
 }
 
