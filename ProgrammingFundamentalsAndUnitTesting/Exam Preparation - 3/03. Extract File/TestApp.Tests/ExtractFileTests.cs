6cd2622bdda545ed8d5614b274bc56c29ecd52df
stykst <k_stoyanov@alice.it>
2023-11-01 15:45:31 +0200

    Add new project

--- ProgrammingFundamentalsAndUnitTesting/Exam Preparation - 3/03. Extract File/TestApp.Tests/ExtractFileTests.cs
+++ ProgrammingFundamentalsAndUnitTesting/Exam Preparation - 3/03. Extract File/TestApp.Tests/ExtractFileTests.cs
@@ -8,30 +8,62 @@ public class ExtractFileTests
     [Test]
     public void Test_GetFile_NullPath_ThrowsArgumentNullException()
     {
-        // TODO: implement the test
+        // Arrange
+        string path = null;
+
+        // Act + Assert
+        Assert.Throws<ArgumentNullException>(() => ExtractFile.GetFile(path));
     }
 
     [Test]
     public void Test_GetFile_EmptyPath_ThrowsArgumentNullException()
     {
-        // TODO: implement the test
+        // Arrange
+        var path = string.Empty;
+
+        // Act + Assert
+        Assert.Throws<ArgumentNullException>(() => ExtractFile.GetFile(path));
     }
 
     [Test]
     public void Test_GetFile_ValidPath_ReturnsFileNameAndExtension()
     {
-        // TODO: implement the test
+        // Arrange
+        var path = "C:\\Users\\user\\Text.txt";
+        var expected = "File name: Text\nFile extension: txt";
+
+        // Act
+        var result = ExtractFile.GetFile(path);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_GetFile_PathWithNoExtension_ReturnsFileNameOnly()
     {
-        // TODO: implement the test
+        // Arrange
+        var path = "C:\\Users\\user\\Text";
+        var expected = "File name: Text";
+
+        // Act
+        var result = ExtractFile.GetFile(path);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_GetFile_PathWithSpecialCharacters_ReturnsFileNameAndExtension()
     {
-        // TODO: implement the test
+        // Arrange
+        var path = "C:\\?Users\\@user\\#Text.txt";
+        var expected = "File name: #Text\nFile extension: txt";
+
+        // Act
+        var result = ExtractFile.GetFile(path);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
--- ProgrammingFundamentalsAndUnitTesting/Exam Preparation - 3/04. Drum Set/TestApp.Tests/DrumSetTests.cs
+++ ProgrammingFundamentalsAndUnitTesting/Exam Preparation - 3/04. Drum Set/TestApp.Tests/DrumSetTests.cs
@@ -10,30 +10,72 @@ public class DrumSetTests
     [Test]
     public void Test_Drum_TerminateCommandNotGiven_ThrowsArgumentException()
     {
-        // TODO: implement the test
+        // Arrange
+        var money = 50.00M;
+        var initialQuality = new List<int>() { 1, 6, 7 };
+        var commands = new List<string> { };
+
+        // Act + Assert
+        Assert.Throws<ArgumentException>(() => DrumSet.Drum(money, initialQuality, commands));
     }
 
     [Test]
     public void Test_Drum_StringGivenAsCommand_ThrowsArgumentException()
     {
-        // TODO: implement the test
+        // Arrange
+        var money = 50.00M;
+        var initialQuality = new List<int>() { 1, 6, 7 };
+        var commands = new List<string> { "ouoiuo" };
+
+        // Act + Assert
+        Assert.Throws<ArgumentException>(() => DrumSet.Drum(money, initialQuality, commands));
     }
 
     [Test]
     public void Test_Drum_ReturnsCorrectQualityAndAmount()
     {
-        // TODO: implement the test
+        // Arrange
+        var money = 50.00M;
+        var initialQuality = new List<int>() { 1, 6, 7 };
+        var commands = new List<string> { "1", "2", "Hit it again, Gabsy!" };
+        var expected = "1 3 4\nGabsy has 44.00lv.";
+
+        // Act
+        var result = DrumSet.Drum(money, initialQuality, commands);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_Drum_BalanceZero_WithOneDrumLeftOver_ReturnsCorrectQualityAndAmount()
     {
-        // TODO: implement the test
+        // Arrange
+        var money = 0M;
+        var initialQuality = new List<int>() { 1, 6, 7 };
+        var commands = new List<string> { "1", "2", "3", "Hit it again, Gabsy!" };
+        var expected = "1\nGabsy has 0.00lv.";
+
+        // Act
+        var result = DrumSet.Drum(money, initialQuality, commands);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_Drum_NotEnoughBalance_RemovesAllDrums_ReturnsCorrectQualityAndAmount()
     {
-        // TODO: implement the test
+        // Arrange
+        var money = 1M;
+        var initialQuality = new List<int>() { 1, 6, 7 };
+        var commands = new List<string> { "1", "2", "3", "3", "3", "Hit it again, Gabsy!" };
+        var expected = "\nGabsy has 1.00lv.";
+
+        // Act
+        var result = DrumSet.Drum(money, initialQuality, commands);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
